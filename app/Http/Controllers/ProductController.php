0bd2f29930dcc3acf8ebfd422d9f4f4922b95a0c
harmlessprince <realolamilekan@gmail.com>
2023-06-09 06:41:43 +0100

    added product service and controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Services\ProductDiscountService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct(protected readonly ProductDiscountService $productDiscountService) {
+      
+    }
+    public function index(Request $request)
+    {
+        $category =  $request->query('category');
+        $priceLessThan =  $request->query('priceLessThan');
+        $perPage = $request->query('per_page', 5);
+
+        $productQuery = Product::query();
+        if ($category != null) $productQuery->where('category', 'LIKE', '%' . $category . '%');
+        if ($priceLessThan != null && is_numeric($priceLessThan)) $productQuery->where('price', '<=', $priceLessThan);
+        $products =  $productQuery->simplePaginate($perPage);
+        foreach ($products as $product){
+            /** @var  Product $product */
+            $product->price = $this->productDiscountService->productPricing($product);
+        }
+        return $products;
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property-read int $id
+ * @property string $name
+ * @property string $sku
+ * @property string $category
+ * @property int|float $price
+ * @property-read  int $original
+ * @property-read  int $final
+ * @property-read $discount_percentage
+ * @property-read $currency
+ */
+
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    protected $hidden = ['created_at', 'updated_at', 'id'];
+    public const CURRENCY = 'EUR';
 }
